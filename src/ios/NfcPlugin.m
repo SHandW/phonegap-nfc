53a38dc048896106dc43136731d4e512380df6b4
SHandW <binhcao7982@gmail.com>
2024-03-06 17:47:48 +0800

    NSNumber

--- src/ios/NfcPlugin.m
+++ src/ios/NfcPlugin.m
@@ -430,7 +430,7 @@
 
 #pragma mark - ISO 15693 Tag functions
 - (void)customCommandISO15:(NFCReaderSession * _Nonnull)session 
-                        flags:(NSNumber)flags 
+                        flags:(NSInteger)flags 
                         code:(NSInteger)code 
                         param:(NSData *)param API_AVAILABLE(ios(13.0)){
     
