03b6364a1e10a73133d08961ee7a978225b24f9f
SHandW <binhcao7982@gmail.com>
2024-03-06 22:15:40 +0800

    debug 32

--- src/ios/NfcPlugin.m
+++ src/ios/NfcPlugin.m
@@ -454,8 +454,8 @@
 
 #pragma mark - ISO 15693 Tag functions
 - (void)customCommandISO15:(NFCReaderSession * _Nonnull)session 
-                        tag:(id<NFCISO15693Tag>)tag 
                         flags:(RequestFlag)flags 
+                        tag:(id<NFCISO15693Tag>)tag 
                         code:(NSInteger)code 
                         param:(NSData *)param API_AVAILABLE(ios(13.0)){
     [tag customCommandWithRequestFlag:flags
