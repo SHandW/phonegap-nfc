fdac663d6fec64fc3b73854332b629a03f408efc
SHandW <binhcao7982@gmail.com>
2024-03-06 20:20:25 +0800

    debug 19

--- src/ios/NfcPlugin.m
+++ src/ios/NfcPlugin.m
@@ -154,9 +154,9 @@
     self.shouldUseTagReaderSession = YES;
     BOOL reusingSession = YES;
     
-    NSArray<NSNumber *> *data = [command argumentAtIndex:0];
+    NSArray *data = [command argumentAtIndex:0];
                               
-    NSData *customRequestParameters = [self uint8ArrayToNSData:[data]];  
+    NSData *customRequestParameters = [self uint8ArrayToNSData:data];  
 
     //self.sessionCallbackId = [command.callbackId copy];
 
