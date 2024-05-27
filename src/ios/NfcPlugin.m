6de8d66129d5870ec6c350d984288ab793e3a28f
SHandW <binhcao7982@gmail.com>
2024-05-27 15:11:05 +0800

    debug 2

--- src/ios/NfcPlugin.m
+++ src/ios/NfcPlugin.m
@@ -364,6 +364,7 @@
 - (void)startScanSession:(CDVInvokedUrlCommand*)command {
     
     self.writeMode = NO;
+    self.commandMode = NO;
     
     NSLog(@"shouldUseTagReaderSession %d", self.shouldUseTagReaderSession);
     NSLog(@"callbackOnSessionStart %d", self.sendCallbackOnSessionStart);
